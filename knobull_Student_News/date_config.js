ff6e1a85e01e57db10f72b9b4283b2d5c638d602
Sujar Henry <80332634+Sujar-Henry@users.noreply.github.com>
2022-06-27 10:49:13 -0400

    Delete knobull_Student_News directory

--- knobull_Student_News/date_config.js
+++
@@ -1,8 +0,0 @@
-const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-var today = new Date();
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0'); 
-var yyyy = today.getFullYear();
-
-today = today.toLocaleString('default', { month: 'long' }) + ' ' + dd;
-document.write(today);
